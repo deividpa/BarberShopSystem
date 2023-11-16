cca4792a31707eaf5e20d853c8f1c2cd592ef564
David Pérez <deividpa022@gmail.com>
2023-11-16 17:42:26 -0500

    Se corrige la relación entre Profesionales y Servicios en los modelos, se crea la inyección de dependencia de Servicios y finalmente, se crean las vistas del CRUD de servicios

---
+++ BarberShopSystem/Controllers/ServiciosController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BarberShopSystem.Models;
+
+namespace BarberShopSystem.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiciosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Servicios
+        public async Task<IActionResult> Index()
+        {
+              return _context.Servicios != null ? 
+                          View(await _context.Servicios.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Servicios'  is null.");
+        }
+
+        // GET: Servicios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // GET: Servicios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servicios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descripcion,Duracion,Precio")] Servicio servicio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(servicio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios.FindAsync(id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+            return View(servicio);
+        }
+
+        // POST: Servicios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descripcion,Duracion,Precio")] Servicio servicio)
+        {
+            if (id != servicio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(servicio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicioExists(servicio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Servicios == null)
+            {
+                return NotFound();
+            }
+
+            var servicio = await _context.Servicios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return View(servicio);
+        }
+
+        // POST: Servicios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Servicios == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Servicios'  is null.");
+            }
+            var servicio = await _context.Servicios.FindAsync(id);
+            if (servicio != null)
+            {
+                _context.Servicios.Remove(servicio);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicioExists(int id)
+        {
+          return (_context.Servicios?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ BarberShopSystem/Services/ServiciosService.cs
@@ -0,0 +1,33 @@
+﻿using BarberShopSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BarberShopSystem.Services
+{
+    public class ServiciosService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ServiciosService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Servicio> ObtenerListaDeServicios()
+        {
+            return _dbContext.Servicios.ToList();
+        }
+
+        public void AgregarServicio(Servicio nuevoServicio)
+        {
+            _dbContext.Servicios.Add(nuevoServicio);
+            _dbContext.SaveChanges();
+        }
+
+        public Servicio ObtenerServicioPorId(int id)
+        {
+            return _dbContext.Servicios.FirstOrDefault(s => s.Id == id) ?? new Servicio();
+        }
+    }
+}
