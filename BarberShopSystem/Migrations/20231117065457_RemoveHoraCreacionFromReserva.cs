4b492647bf7ac50b05ddea70d54f64ea8dfdaad3
David Pérez <deividpa022@gmail.com>
2023-11-17 03:02:57 -0500

    Se elimina campo HoraCreacion de la tabla Reserva

---
+++ BarberShopSystem/Migrations/20231117065457_RemoveHoraCreacionFromReserva.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberShopSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveHoraCreacionFromReserva : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HoraCreacion",
+                table: "Reservas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HoraCreacion",
+                table: "Reservas",
+                type: "datetime(6)",
+                nullable: true);
+        }
+    }
+}
