48c27a2b30ffb10d83c9cccbf811ef95b3495ba1
David Pérez <deividpa022@gmail.com>
2023-11-17 01:40:02 -0500

    Se crea una nueva migracion, incluyendo la fecha de los cupos

---
+++ BarberShopSystem/Migrations/20231117055007_AgregarCampoFechaACupos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberShopSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCampoFechaACupos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Fecha",
+                table: "Cupos",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fecha",
+                table: "Cupos");
+        }
+    }
+}
